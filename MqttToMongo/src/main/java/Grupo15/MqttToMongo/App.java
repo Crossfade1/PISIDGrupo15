e8f694b641c6bcd3ed7c0b96b10232491083460b
Crossfade1 <rui.c.2k11@gmail.com>
2023-04-19 00:04:44 +0100

    first commit

---
+++ MqttToMongo/src/main/java/Grupo15/MqttToMongo/App.java
@@ -0,0 +1,13 @@
+package Grupo15.MqttToMongo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
